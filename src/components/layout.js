60f90b1b1f243402e3c5ddd6d082069beb43d3b8
Niels <niels.reijnders@edu-kw1c.nl>
2021-02-13 16:46:39 +0100

    Fix smoothscroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './base.css';
 import Smooth from 'smooth-scrolling';
 import Container from './container';
@@ -6,13 +6,13 @@ import Navigation from './navigation';
 import Footer from './Footer';
 
 const Template = ({ children }) => {
-  const section = document.querySelector('.vs-section');
+  const section = useRef(null);
 
   useEffect(() => {
     const smooth = new Smooth({
-      native: false,
-      section,
-      ease: 0.05,
+      native: true,
+      section: section.current,
+      ease: 0.1,
     });
 
     smooth.init();
@@ -20,7 +20,7 @@ const Template = ({ children }) => {
 
   return (
     <Container>
-      <div className="vs-section">
+      <div ref={section} className="vs-section">
         <Navigation />
         {children}
         <Footer />
