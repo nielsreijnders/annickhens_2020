7a5aeec885f8f2bff9dd8651c47a6930688a3842
Niels <niels.reijnders@edu-kw1c.nl>
2021-02-13 16:34:13 +0100

    Add smooth scroll

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './base.css';
+import Smooth from 'smooth-scrolling';
 import Container from './container';
 import Navigation from './navigation';
 import Footer from './Footer';
 
-const Template = ({ children }) => (
-  <Container>
-    <Navigation />
-    {children}
-    <Footer />
-  </Container>
-);
+const Template = ({ children }) => {
+  const section = document.querySelector('.vs-section');
+
+  useEffect(() => {
+    const smooth = new Smooth({
+      native: false,
+      section,
+      ease: 0.05,
+    });
+
+    smooth.init();
+  }, []);
+
+  return (
+    <Container>
+      <div className="vs-section">
+        <Navigation />
+        {children}
+        <Footer />
+      </div>
+    </Container>
+  );
+};
 
 export default Template;
